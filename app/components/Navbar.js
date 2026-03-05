6c31b863e21089050908e2ec92c11e58c841c593
Duong Tri Thien Phung <dphung@my.ggu.edu>
2026-03-05 22:36:25 +0700

    Update Navbar: use Axiom Realty logo image
    Replaced icon with an image logo in the Navbar component and simplified Link elements.

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,6 +1,6 @@
 'use client';
-
 import Link from 'next/link';
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
@@ -13,28 +13,26 @@ export default function Navbar() {
     <nav className="navbar">
       <div className="container nav-inner">
         <Link href="/" className="logo">
-          <i className="fa-solid fa-house"></i> Prime Listings
+          <Image
+            src="/AXIOM LLC Logo White.png"
+            alt="Axiom Realty"
+            width={140}
+            height={50}
+            style={{ objectFit: 'contain' }}
+          />
         </Link>
         <ul className="nav-links">
           <li>
-            <Link href="/" className={navClass('/')}>
-              Home
-            </Link>
+            <Link href="/" className={navClass('/')}>Home</Link>
           </li>
           <li>
-            <Link href="/real-estate" className={navClass('/real-estate')}>
-              Real Estate
-            </Link>
+            <Link href="/real-estate" className={navClass('/real-estate')}>Real Estate</Link>
           </li>
           <li>
-            <Link href="/used-items" className={navClass('/used-items')}>
-              Used Items
-            </Link>
+            <Link href="/used-items" className={navClass('/used-items')}>Used Items</Link>
           </li>
           <li>
-            <Link href="/contact" className={navClass('/contact')}>
-              Contact
-            </Link>
+            <Link href="/contact" className={navClass('/contact')}>Contact</Link>
           </li>
         </ul>
       </div>
