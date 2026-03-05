8d863693108c25b02583e25a4b115ae28e432ad7
Duong Tri Thien Phung <dphung@my.ggu.edu>
2026-03-05 10:33:00 +0700

    Add app/components/Navbar.js

---
+++ app/components/Navbar.js
@@ -0,0 +1,43 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export default function Navbar() {
+  const pathname = usePathname();
+
+  const navClass = (path) =>
+    pathname === path ? 'nav-links-a active' : 'nav-links-a';
+
+  return (
+    <nav className="navbar">
+      <div className="container nav-inner">
+        <Link href="/" className="logo">
+          <i className="fa-solid fa-house"></i> Prime Listings
+        </Link>
+        <ul className="nav-links">
+          <li>
+            <Link href="/" className={navClass('/')}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/real-estate" className={navClass('/real-estate')}>
+              Real Estate
+            </Link>
+          </li>
+          <li>
+            <Link href="/used-items" className={navClass('/used-items')}>
+              Used Items
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" className={navClass('/contact')}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
