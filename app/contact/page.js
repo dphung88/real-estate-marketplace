a200eb83b62fa44093e3eb67f4c6e167233b9b3e
Duong Tri Thien Phung <dphung@my.ggu.edu>
2026-03-05 22:39:51 +0700

    Rebrand contact page: Prime Listings -> Axiom Realty

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,7 +2,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 export const metadata = {
-  title: 'Contact Us - Prime Listings',
+  title: 'Contact Us - Axiom Realty',
 };
 
 export default function ContactPage() {
